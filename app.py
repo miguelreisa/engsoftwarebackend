bdc20e93cf67788f41e96ca229825ac6fb5a7c12
miguelreisa <miguelreisa@users.noreply.github.com>
2018-05-22 10:51:03 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,17 +3,17 @@ import os
 
 from flask import Flask
 from flask_restful import Api
-from flask_jwt import JWT
 
-from security import authenticate, identity #nosso
-from resources.user import UserRegister
-from resources.item import Item, ItemList
-from resources.store import Store,StoreList
+from resources.user import UserRegister, UserLogin
+from resources.conference import Conference, ConferenceList
+from resources.paper import Paper
+from resources.pcmember import PCMember
+from resources.thread import Thread, ThreadResponses
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db') #dizer ao sqlalchemy onde encontrar o ficheiro .db (aqui dizemos que esta na root folder do projecto). Nao precisa de ser sqlite, pode ser mysql, etc
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #coiso
-app.secret_key = 'miguel' #teria que ser secreto & seguro
+app.secret_key = 'engsoftware' 
 api = Api(app)
 
 #cria um novo endpoint /auth em que se envia username e password e sao levados ao metodo authenticate feito por nos, se der match(ver funcao authenticate), devolve o user (na func authenticate)
@@ -21,11 +21,14 @@ api = Api(app)
 #se conseguir fizer isso é porque o user esta autenticado e pode fazer o request
 jwt = JWT(app, authenticate, identity)
 
-api.add_resource(Store, '/store/<string:name>')
-api.add_resource(Item, '/item/<string:name>')
-api.add_resource(ItemList, '/items')
-api.add_resource(StoreList, '/stores')
 api.add_resource(UserRegister, '/register')
+api.add_resource(UserLogin, '/login')
+api.add_resource(Conference, '/conference')
+api.add_resource(ConferenceList, '/conferences')
+api.add_resource(Paper, '/paper')
+api.add_resource(PCMember, '/pcmember')
+api.add_resource(Thread, '/thread')
+api.add_resource(ThreadResponses, '/thread/<int:threadId>')
 
 #quando se corre app.run da o nome a __name__ '__main__'
 #este if serve para se este ficheiro app2.py for importado noutro, nao queremos que seja corrido outra vez, ou seja, que faça start do web server
